ce6e8028f479fb0e9d2a2a89ac0392232b0260c9
ezequielcastr0 <ezequielcastro@isft220.edu.ar>
2024-10-06 16:13:17 -0300

    Add files via upload

--- agr.js
+++ agr.js
@@ -1,44 +1,53 @@
-// Clase Inventario
-class Inventario {
-    constructor() {
-      this.productos = [];
-    }
-  
-    // Método para agregar productos al inventario
-    agregarProducto(nombre, precio, cantidad) {
-      this.productos.push({ nombre, precio, cantidad });
-    }
-  
-    // Método para mostrar el inventario completo
-    mostrarInventario() {
-      for (let producto of this.productos) {
-        document.getElementById('resultado').innerText = `Pedido aleatorio: ${producto.nombre},${producto.precio},${producto.cantidad}`;
-        
-      }
-    }
-  
-    // Método para calcular el valor total del inventario
-    calcularValorTotalInventario() {
-      let valorTotal = 0;
-      for (let producto of this.productos) {
-        valorTotal += producto.precio * producto.cantidad;
-      }
-      return valorTotal;
-    }
-    
+class Producto {
+  constructor() {
+    this.productos = [];
   }
+
+  // Método para agregar productos al inventario
+  agregarProducto(nombre, precio) {
+    this.productos.push({ nombre, precio});
+  }
+
+  // Método para mostrar el inventario completo
+  mostrarInventario() {
+    console.log("Inventario actual:");
+    for (let producto of this.productos) {
+      console.log(`Producto: ${producto.nombre}, Precio: ${producto.precio}`);
+    }
+  }
+  random() {
+    var aleatorio = Math.floor(Math.random() *this.productos.length);
+    const productoAleatorio =this.productos[aleatorio];
+    console.log(`Producto aleatorio: ${productoAleatorio.nombre}, Precio: ${productoAleatorio.precio}`);
+    document.getElementById('resultado').innerHTML = `Producto aleatorio: ${productoAleatorio.nombre} - Precio: ${productoAleatorio.precio}`;
+}
+  // Método para calcular el valor total del inventario
+  calcularValorTotalInventario() {
+    let valorTotal = 0;
+    for (let producto of this.productos) {
+      valorTotal += producto.precio * producto.cantidad;
+    }
+    return valorTotal;
+  }
+
+}
+
+// Ejecución
+let inventarioTienda = new Producto();
+
+// Agregar productos al inventario
+inventarioTienda.agregarProducto("Pizza",200);
+inventarioTienda.agregarProducto("Manaos",200);
+inventarioTienda.agregarProducto("Agua",100);
+
+// Mostrar inventario inicial
+inventarioTienda.mostrarInventario();
+inventarioTienda.random();
+
+
+
+
+  //https://github.com/configuroweb/nombres/blob/master/index.html// Fuente y codigo base
+  //https://jorgesanchez.net/manuales/html/tablas-html.html#google_vignette//
+  //https://www.youtube.com/watch?v=5DaZXXbHI_U//
   
-  // Ejecución
-  let inventarioTienda = new Inventario();
-  
-  // Agregar productos al inventario
-  inventarioTienda.agregarProducto("Pizza NAPO", 1, 10000);
-  inventarioTienda.agregarProducto("COCA COLA", 2, 3000);
-  inventarioTienda.agregarProducto("AGUA SABORIZADA", 1, 2000);
-  
-  // Mostrar inventario inicial
-  inventarioTienda.mostrarInventario();
-  
-  // Calcular y mostrar el valor total del inventario
-  let valorTotal = inventarioTienda.calcularValorTotalInventario();
-  console.log(`Valor total del inventario: ${valorTotal}`);
